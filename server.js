81455a50c251c9b7ad3ede87684d0987ce3ec914
ShwethaTW <shwetha.e@kissflow.com>
2025-08-17 22:37:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const openai = new OpenAI({
 // Pinecone client
 const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
-  environment: process.env.PINECONE_ENVIRONMENT, 
 });
 
 // reference your index
